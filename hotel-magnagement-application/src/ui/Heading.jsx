9bb42e6224c497a9ed437cbf33cc90ad44103b71
nde booy <ndesimoniche@gmail.com>
2024-03-27 19:39:19 +0400

    fixed style issues

--- hotel-magnagement-application/src/ui/Heading.jsx
+++ hotel-magnagement-application/src/ui/Heading.jsx
@@ -1,7 +1,29 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+// const test = `${5 > 7 && "background-color:purple"}`;
 
 const Heading = styled.h1`
-  display: flex;
+  ${(props) =>
+    props.as === "h1" &&
+    css`
+      background-color: yellow;
+    `}
+
+  ${(props) =>
+    props.as === "h2" &&
+    css`
+      background-color: green;
+    `}
+
+    ${(props) =>
+    props.as === "h3" &&
+    css`
+      background-color: pink;
+    `}
+
+
+  font-size: 30px;
+  font-weight: 600;
 `;
 
 export default Heading;
