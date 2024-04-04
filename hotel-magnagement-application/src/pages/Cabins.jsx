45de4098a2830f788a731440a292c577e6caa7db
nde booy <ndesimoniche@gmail.com>
2024-04-04 19:16:35 +0400

    addad the data

--- hotel-magnagement-application/src/pages/Cabins.jsx
+++ hotel-magnagement-application/src/pages/Cabins.jsx
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import supabase from "../services/supabase";
 
 function Cabins() {
+  useEffect(() => {
+    async function apirequest() {
+      const response = supabase().then((data) => console.log(data));
+      return response;
+    }
+    apirequest();
+  }, []);
+
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
